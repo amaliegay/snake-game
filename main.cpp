e16fe0ccc660a9dbfeba0f7e624b9fcecfa67ac8
Amalie <amaliegay@outlook.com>
2023-12-09 23:38:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 <<<<<<< HEAD
 =======
 
+<<<<<<< HEAD
 #define N 20
 #define HEIGHT N
 #define WIDTH N
 >>>>>>> parent of 355905d (split up main.cpp)
 
+=======
+>>>>>>> parent of af895d8 (Update main.cpp:Game(WIDTH, HEIGHT))
 int main()
 {
 	Game game = Game();
